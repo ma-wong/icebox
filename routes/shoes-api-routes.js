2a7cee5a53b47e1c18c00f84d58e3e4d13d97321
ma-wong <73137595+ma-wong@users.noreply.github.com>
2021-02-25 14:21:07 -0800

    us, eu, and uk size filters now work (#3)

--- routes/shoes-api-routes.js
+++ routes/shoes-api-routes.js
@@ -28,46 +28,86 @@ module.exports = function(app) {
     app.post("/api/shoes", function(req, res) {
         let whereProps = {};
 
+        // where brands matches filters
         if (req.body.brands) {
             whereProps.brand = {
                 [Op.or]: req.body.brands
             }
         }
 
+        // where collaborators matches filters
         if (req.body.collabs) {
             whereProps.collaborators = {
                 [Op.or]: req.body.collabs
             }
         }
 
+        // where colors matches filters
         if (req.body.colors) {
             whereProps.color = {
                 [Op.or]: req.body.colors
             }
         }
 
+        // where geder matches filters
         if (req.body.gender) {
             whereProps.sizing_gender = {
                 [Op.or]: req.body.gender
             }
         }
 
+        // where styles matches filters
         if (req.body.styles) {
             whereProps.style = {
                 [Op.or]: req.body.styles
             }
         }
 
+        // where max price matches filters
         if (req.body.max_price) {
             whereProps.price_max = {
                 [Op.lte]: req.body.max_price
             }
         }
 
+        // where release date matches filters
         if (req.body.years) {
             whereProps.release_date = {
                 [Op.regexp]: req.body.years.join("|")
             }
+        }
+
+        // where US size matches filters
+        if (req.body.us_size) {
+            whereProps.min_size = {
+                [Op.lte]: req.body.us_size
+            },
+            whereProps.max_size = {
+                [Op.gte]: req.body.us_size
+            },
+            whereProps.sizing_type = "US"
+        }
+
+        // where EU size matches filters
+        if (req.body.eu_size) {
+            whereProps.min_size = {
+                [Op.lte]: req.body.eu_size
+            },
+            whereProps.max_size = {
+                [Op.gte]: req.body.eu_size
+            },
+            whereProps.sizing_type = "EU"
+        }
+
+        // where UK size matches filters
+        if (req.body.uk_size) {
+            whereProps.min_size = {
+                [Op.lte]: req.body.uk_size
+            },
+            whereProps.max_size = {
+                [Op.gte]: req.body.uk_size
+            },
+            whereProps.sizing_type = "UK"
         }
 
         
@@ -93,153 +133,3 @@ module.exports = function(app) {
 
   
 };
-
-
-// // FINDING SHOES IN SPECIFIC CATEGORIES
-// // Finding all shoes with a specific name
-// app.get("/api/shoes/name/:name", function(req, res) {
-//     db.Shoe.findAll({
-//         where: {
-//             name: req.params.name
-//         }
-//     }).then(function(dbShoe) {
-//         res.json(dbShoe);
-//     });
-// });
-
-// // finding all shoes with a specific brand
-// app.get("/api/shoes/brand/:brand", function(req, res) {
-//     db.Shoe.findAll({
-//         where: {
-//             brand: req.params.brand
-//         }
-//     }).then(function(dbShoe) {
-//         res.json(dbShoe);
-//     });
-// });
-
-// // finding all shoes with a specific release date
-// app.get("api/shoes/release_date/:release_date", function(req, res) {
-//     db.Shoe.findAll({
-//         where: {
-//             release_date: req.params.release_date
-//         }
-//     }).then(function(dbShoe) {
-//         res.json(dbShoe);
-//     });
-// });
-
-// // finding all shoes with specific collaborators
-// app.get("/api/shoes/collaborators/:collaborators", function(req, res) {
-//     db.Shoe.findAll({
-//         where: {
-//             collaborators: req.params.collaborators
-//         }
-//     }).then(function(dbShoe) {
-//         res.json(dbShoe);
-//     });
-// });
-
-// // finding all shoes with a specific color
-// app.get("/api/shoes/color/:color", function(req, res) {
-//     db.Shoe.findAll({
-//         where: {
-//             color: req.params.color
-//         }
-//     }).then(function(dbShoe) {
-//         res.json(dbShoe);
-//     });
-// });
-
-// // finding all shoes with a specific sizing_type
-// app.get("/api/shoes/sizing_type/:sizing_type", function(req, res) {
-//     db.Shoe.findAll({
-//         where: {
-//             sizing_type: req.params.sizing_type
-//         }
-//     }).then(function(dbShoe) {
-//         res.json(dbShoe);
-//     });
-// });
-
-// // finding all shoes with a specific sizing_gender
-// app.get("/api/shoes/sizing_gender/:sizing_gender", function(req, res) {
-//     db.Shoe.findAll({
-//         where: {
-//             sizing_gender: req.params.sizing_gender
-//         }
-//     }).then(function(dbShoe) {
-//         res.json(dbShoe);
-//     });
-// });
-
-// // finding all shoes with a specific min_size
-// app.get("/api/shoes/min_size/:min_size", function(req, res) {
-//     db.Shoe.findAll({
-//         where: {
-//             min_size: req.params.min_size
-//         }
-//     }).then(function(dbShoe) {
-//         res.json(dbShoe);
-//     });
-// });
-
-// // finding all shoes with a specific max_size
-// app.get("/api/shoes/max_size/:max_size", function(req, res) {
-//     db.Shoe.findAll({
-//         where: {
-//             max_size: req.params.max_size
-//         }
-//     }).then(function(dbShoe) {
-//         res.json(dbShoe);
-//     });
-// });
-
-// // finding all shoes with a specific price range
-// app.get("/api/shoes/price_range/:price_min/:price_max", function(req, res) {
-//     db.Shoe.findAll({
-//         where: {
-//             price_min: {
-//                 [Op.gte]: req.params.price_min
-//             },
-//             price_max: {
-//                 [Op.lte]: req.params.price_max
-//             }
-//         }
-//     }).then(function(dbShoe) {
-//         res.json(dbShoe);
-//     });
-// });
-
-// // finding all shoes with a specific description
-// app.get("/api/shoes/description/:description", function(req, res) {
-//     db.Shoe.findAll({
-//         where: {
-//             description: req.params.description
-//         }
-//     }).then(function(dbShoe) {
-//         res.json(dbShoe);
-//     });
-// });
-
-// // finding all shoes with a specific style
-// app.get("/api/shoes/style/:style", function(req, res) {
-//     db.Shoe.findAll({
-//         where: {
-//             style: req.params.style
-//         }
-//     }).then(function(dbShoe) {
-//         res.json(dbShoe);
-//     });
-// });
-
-// // finding all shoes with a specific product_link
-// app.get("/api/shoes/product_link/:product_link", function(req, res) {
-//     db.Shoe.findAll({
-//         where: {
-//             product_link: req.params.product_link
-//         }
-//     }).then(function(dbShoe) {
-//         res.json(dbShoe);
-//     });
-// });
